17abe19050a9a3eb7a83437f8170bb923d8619a9
dimaweb1987 <em15@yandex.ru>
2017-11-19 17:42:24 +0300

    added

--- index.php
+++ index.php
@@ -3,14 +3,7 @@
 $arr = array("hello", "my name is", "the best", "programmer");
 foreach ($arr as string){
     
-    echo $arr[0];
-    echo "<p>";
-    echo $arr[1];
-    echo "<p>";
-    echo $arr[2];
-    echo "<p>";
-    echo $arr[3];
-    echo "<p>";
+   echo "<p>".string."</p>";
     
     
     
