753f6cb68915e9c1f8e41c27f45b4e00d85b8b7b
dimaweb1987 <em15@yandex.ru>
2017-11-19 17:40:56 +0300

    added

--- index.php
+++ index.php
@@ -1,6 +1,20 @@
 <?php
 
-echo "hello";
-echo "from there";
+$arr = array("hello", "my name is", "the best", "programmer");
+foreach ($arr as string){
+    
+    echo $arr[0];
+    echo "<p>";
+    echo $arr[1];
+    echo "<p>";
+    echo $arr[2];
+    echo "<p>";
+    echo $arr[3];
+    echo "<p>";
+    
+    
+    
+}
+
 
 ?>
